c251f487872302e761ab1ad4e02515c0eb02304f
hanilucky <hanilucky@163.com>
2017-12-18 11:03:45 +0800

    定时发送邮件+脚本启动代码提交

--- com.covet.quartz.mail.shell.demo/src/main/resources/run/startup.bat
+++ com.covet.quartz.mail.shell.demo/src/main/resources/run/startup.bat
@@ -2,4 +2,4 @@
 setLocal enableDelayedExpansion
 set cp=.
 FOR %%i IN ("%~dp0libs\*.jar") DO set cp=!cp!;%%~fsi
-start "com.covet.quartz.mail.shell.demo" java -server -cp %cp% com.covet.timer.shell.Shell
+start "quartz.sendmailjob" java -server -cp %cp% com.covet.timer.shell.Shell
