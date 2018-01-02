7ccd9fce72f178b74d2611aada9e0c4475fccc94
hanilucky <hanilucky@163.com>
2018-01-02 22:20:50 +0800

    dubbo工程部分修改提交

--- com.covet.spring.dubbo.demo/com.covet.spring.dubbo.consumer/src/test/java/com/covet/test/SayHelloWorldTest.java
+++ com.covet.spring.dubbo.demo/com.covet.spring.dubbo.consumer/src/test/java/com/covet/test/SayHelloWorldTest.java
@@ -1,20 +1,20 @@
 package com.covet.test;
 
+import java.io.IOException;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.covet.service.SayHelloWorld;
 
 public class SayHelloWorldTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
 		context.start();
 		SayHelloWorld bean = context.getBean(SayHelloWorld.class);
 		String sayHelloWorld = bean.sayHelloWorld();
 		System.err.println("ok"+sayHelloWorld);
-		while (true) {
-
-		}
+		System.in.read();
 	}
 }
--- com.covet.spring.dubbo.demo/com.covet.spring.dubbo.provide/src/test/java/com/covet/spring/dubbo/provide/SayHelloWorldTest.java
+++ com.covet.spring.dubbo.demo/com.covet.spring.dubbo.provide/src/test/java/com/covet/spring/dubbo/provide/SayHelloWorldTest.java
@@ -1,16 +1,16 @@
 package com.covet.spring.dubbo.provide;
 
+import java.io.IOException;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SayHelloWorldTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
 		context.start();
 
-		while (true) {
-
+		System.in.read();
 		}
-	}
 }
